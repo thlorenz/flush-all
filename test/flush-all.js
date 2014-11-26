a706f709107bc9a1a60f9c835dd28969c0eb6f07
Thorsten Lorenz <thlorenz@gmx.de>
2014-11-25 21:32:30 -0500

    accounting for fact that Linux likes to flush

--- test/flush-all.js
+++ test/flush-all.js
@@ -5,6 +5,7 @@
 
 var test = require('tap').test
   , fs = require('fs')
+  , os = require('os')
   , flushAll = require('../')
 
 test('\nflushing will cause perf file to be flushed', function (t) {
@@ -15,12 +16,23 @@ test('\nflushing will cause perf file to be flushed', function (t) {
   t.ok(fs.existsSync(mapFile), 'map file exists')
 
   stat = fs.statSync(mapFile);
-  t.equal(stat.size, 0, 'and its size is 0 since it has not been flushed yet')
+
+  var firstSize = stat.size;
+  // Linux flushes too aggressively 
+  if (os.platform() === 'darwin') 
+    t.equal(firstSize, 0, 'and on darwin its size is 0 since it has not been flushed yet')
 
   flushAll();
 
   stat = fs.statSync(mapFile);
-  t.notEqual(stat.size, 0, 'after I flush all, the map file size is no longer 0 since it has been flushed')
+  // The below by itself is not really a proof since just calling 'flushAll' causes more JIT code to be generated which by 
+  // itself would increase the map file size.
+  // However since Linux flushes more aggressively this is as good as we can do there.
+  // On darwin, we can show that before flushAll nothing got flushed and afterwards it did which is the 
+  // actual proof that this works.
+  t.ok(stat.siz > firstSize, 'after I flush all, the map file size is larger than before and non-zero since has been flushed')
 
+  console.log('size before flushAll()', firstSize);
+  console.log('size after  flushAll()', stat.size);
   t.end()
 })
